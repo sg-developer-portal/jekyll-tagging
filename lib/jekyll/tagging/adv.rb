c40ce2ed1dd28659989a4dc30926f1885afde9fe
Raymond Lei <raymond_lei@tech.gov.sg>
2024-01-11 21:46:35 +0800

    Initial setup

---
+++ lib/jekyll/tagging/adv.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "adv/version"
+
+module Jekyll
+  module Tagging
+    module Adv
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
